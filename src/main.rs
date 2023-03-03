2f1773ddfe2f6fe6d14175abaaeff197057029d9
Sylvan Smit <sjsmit@amazon.com>
2023-03-03 15:11:31 +0100

    wip, deciding if i should rustify now

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
+
+fn ask_prompt(stdout: &std::io::Stdout) {
+
+}
+fn get_input(stdin: &std::io::Stdin) -> Option<String> {
+    let mut buffer = String::new();
+    stdin.read_line(&mut buffer).ok()?;
+    Some(buffer)
+}
+
+
 fn main() {
+    // "Global" variables
+    let mut input = "look around".to_string();
+    let stdin = std::io::stdin();
+
     println!("Welcome to the Rusty Gate Adventure.");
     println!("Unsurprisingly, you are faced with rusty gate.");
+
+
+    // Get input
+
+    // Parse input
+    // Execute
+
     println!("");
     println!("Bye!");
 }
