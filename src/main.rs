8b22e8abfcb414495b36770cffa5b8585956ed21
Sylvan Smit <sjsmit@amazon.com>
2023-03-17 15:54:35 +0100

    wip - trying to rustify objects

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::io::Write;
 
 #[derive(Debug)]
-struct Location<'a> {
+struct Object<'a> {
     description: &'a str,
     tag: Noun,
+    location: Option<&'a Object<'a>>,
 }
 
 #[derive(Debug)]
@@ -13,19 +14,23 @@ enum Verb {
     Go,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Noun {
     Around,
     Field,
     Cave,
+    Silver,
+    Gold,
+    Guard,
+    Yourself,
     Unknown,
 }
 
 type Action = (Option<Verb>, Option<Noun>);
 
-struct GameState {
-    location_index: usize,
-}
+// struct GameState {
+//     location_index: usize,
+// }
 
 fn parse_input(input: &str) -> (Option<Verb>, Option<Noun>) {
     let mut words = input.split_whitespace();
@@ -46,26 +51,27 @@ fn parse_input(input: &str) -> (Option<Verb>, Option<Noun>) {
     }
 }
 
-fn execute(game_state: &mut GameState, action: Action) -> String {
+fn execute(action: Action) -> String {
     match action {
         (None, None) => "nothing to do".to_string(),
         (None, Some(_)) => panic!("this an invalid state."),
         (Some(Verb::Quit), _) => "quit".to_string(),
-        (Some(Verb::Look), Some(Noun::Around)) => format!(
-            "you are in {}",
-            LOCATIONS[game_state.location_index].description
-        ),
+        (Some(Verb::Look), Some(Noun::Around)) => {
+            format!("you are in {}", unsafe { PLAYER.description })
+        }
         (Some(Verb::Look), _) => "what exactly would you like to see?".to_string(),
         (Some(Verb::Go), Some(noun)) => {
-            if noun == LOCATIONS[game_state.location_index].tag {
+            if noun == unsafe { PLAYER.location.unwrap().tag } {
                 "you can't get much closer than this".to_string()
             } else {
                 let mut output = "that ain't a place".to_string();
-                for (index, location) in LOCATIONS.iter().enumerate() {
-                    if noun == location.tag {
-                        game_state.location_index = index;
-                        output = "ok, got it".to_string();
-                        break;
+                unsafe {
+                    for location in OBJECTS.iter() {
+                        if noun == location.tag {
+                            PLAYER.location = Some(location);
+                            output = "ok, got it".to_string();
+                            break;
+                        }
                     }
                 }
                 output
@@ -76,22 +82,50 @@ fn execute(game_state: &mut GameState, action: Action) -> String {
 }
 
 // Game State
-const LOCATIONS: [Location; 2] = [
-    Location {
-        description: "an open field",
-        tag: Noun::Field,
-    },
-    Location {
-        description: "a little cave",
-        tag: Noun::Cave,
-    },
-];
 
 fn main() {
     // "Global" variables
     let stdin = std::io::stdin();
-    let mut game_state = GameState { location_index: 0 };
 
+     let mut OBJECTS: [Object; 6] = [
+        Object {
+            description: "an open field",
+            tag: Noun::Field,
+            location: None,
+        },
+        Object {
+            description: "a little cave",
+            tag: Noun::Cave,
+            location: None,
+        },
+        Object {
+            description: "a silver coin",
+            tag: Noun::Silver,
+            location: Some(FIELD),
+        },
+        Object {
+            description: "a gold coin",
+            tag: Noun::Gold,
+            location: Some(CAVE),
+        },
+        Object {
+            description: "a burly guard",
+            tag: Noun::Guard,
+            location: Some(FIELD),
+        },
+        Object {
+            description: "yourself",
+            tag: Noun::Yourself,
+            location: Some(FIELD),
+        },
+    ];
+
+    let FIELD: &Object = unsafe { &OBJECTS[0] };
+    let CAVE: &Object = unsafe { &OBJECTS[1] };
+    let SILVER: &Object = unsafe { &OBJECTS[2] };
+    let GOLD: &Object = unsafe { &OBJECTS[3] };
+    let GUARD: &Object = unsafe { &OBJECTS[4] };
+    let mut PLAYER: &mut Object = unsafe { &mut OBJECTS[5] };
     println!("Welcome to the Rusty Gate Adventure.");
     println!("unsurprisingly, you are faced with a rusty gate");
 
@@ -108,7 +142,7 @@ fn main() {
         let action = parse_input(&input);
 
         // Execute
-        let response = execute(&mut game_state, action);
+        let response = execute(action);
 
         // Print response
         if response.eq_ignore_ascii_case("quit") {
